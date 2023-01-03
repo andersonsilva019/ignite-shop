022437912205dd71d29c34e77717239c0f6df906
Anderson <anderson@pop-os.localdomain>
2023-01-03 14:32:50 -0300

    Adicionando carrossel

--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -1,4 +1,6 @@
+import 'keen-slider/keen-slider.min.css'
 import Image from 'next/image'
+import { useKeenSlider } from 'keen-slider/react'
 
 import camiseta1 from '../../assets/camisetas/1.png';
 import camiseta2 from '../../assets/camisetas/2.png';
@@ -7,17 +9,39 @@ import camiseta3 from '../../assets/camisetas/3.png';
 import * as S from './styles'
 
 export function HomePage(){
+
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48
+    }
+  })
+
   return (
-    <S.HomeContainer>
-      <S.Product>
+    <S.HomeContainer ref={sliderRef} className="keen-slider">
+      <S.Product className="keen-slider__slide">
         <Image src={camiseta1} width={520} height={480} alt="" />
         <footer>
           <strong>Camiseta ...</strong>
           <span>R$ 120,00</span>
         </footer>
       </S.Product>
-      <S.Product>
+      <S.Product className="keen-slider__slide">
         <Image src={camiseta2} width={520} height={480} alt="" />
+        <footer>
+          <strong>Camiseta ...</strong>
+          <span>R$ 120,00</span>
+        </footer>
+      </S.Product>
+      <S.Product className="keen-slider__slide">
+        <Image src={camiseta3} width={520} height={480} alt="" />
+        <footer>
+          <strong>Camiseta ...</strong>
+          <span>R$ 120,00</span>
+        </footer>
+      </S.Product>
+      <S.Product className="keen-slider__slide">
+        <Image src={camiseta3} width={520} height={480} alt="" />
         <footer>
           <strong>Camiseta ...</strong>
           <span>R$ 120,00</span>
