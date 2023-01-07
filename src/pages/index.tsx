9fab747c1e2c7babbd889fdea58622ba3abaa80c
Anderson <anderson@pop-os.localdomain>
2023-01-07 09:24:03 -0300

    Melhorando a busca dos produtos

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,12 +3,17 @@ import Stripe from "stripe";
 import { stripe } from "../lib/stripe";
 import { HomePage } from "../templates/Home";
 
-interface Product {
+enum Currency {
+  BRL = "BRL",
+  USD = "USD",
+}
+
+export interface Product {
   id: string
   name: string
   description: string
   imageUrl: string
-  price: number
+  price: string
 }
 
 export interface HomeProps {
@@ -23,27 +28,42 @@ export default function Home(props: HomeProps) {
 
 export const getStaticProps: GetStaticProps = async () => {
 
-  const response = await stripe.products.list({
-    expand: ['data.default_price']
-  })
+  try {
+    const response = await stripe.products.list({
+      expand: ['data.default_price'],
+      active: true,
+    })
 
-  const products = response.data.map(product => {
+    const products = response.data.map(product => {
 
-    const price = product.default_price as Stripe.Price
+      const price = product.default_price as Stripe.Price
+
+      const formattedPrice = new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: Currency.BRL
+      }).format(price.unit_amount ? price.unit_amount / 100 : 0)
+
+
+      return {
+        id: product.id,
+        name: product.name,
+        description: product.description,
+        imageUrl: product.images[0],
+        price: formattedPrice
+      }
+    })
 
     return {
-      id: product.id,
-      name: product.name,
-      description: product.description,
-      imageUrl: product.images[0],
-      price: price.unit_amount ? price.unit_amount / 100 : 0
+      props: {
+        products
+      },
+      revalidate: 60 * 60 * 1 // 1 hour
     }
-  })
+  } catch (error) {
+    console.log(error)
 
-  return {
-    props: {
-      products
-    },
-    revalidate: 60 * 60 * 1 // 1 hours
+    return {
+      notFound: true
+    }
   }
 }
